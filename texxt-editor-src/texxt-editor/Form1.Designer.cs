fb5be8ce39916a5de2115ac9e37dcfd16998f05c
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-02-08 20:33:00 +0000

    feat: open, save etc

--- texxt-editor-src/texxt-editor/Form1.Designer.cs
+++ texxt-editor-src/texxt-editor/Form1.Designer.cs
@@ -29,21 +29,38 @@ namespace texxt_editor
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbEditor = new System.Windows.Forms.TextBox();
+            this.tbEditor = new System.Windows.Forms.RichTextBox();
+            this.lblFile = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbEditor
             // 
+            this.tbEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tbEditor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(2)))));
             this.tbEditor.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tbEditor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tbEditor.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbEditor.ForeColor = System.Drawing.Color.White;
+            this.tbEditor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
             this.tbEditor.Location = new System.Drawing.Point(10, 10);
-            this.tbEditor.Multiline = true;
             this.tbEditor.Name = "tbEditor";
-            this.tbEditor.Size = new System.Drawing.Size(780, 430);
+            this.tbEditor.Size = new System.Drawing.Size(780, 407);
             this.tbEditor.TabIndex = 0;
+            this.tbEditor.Text = "";
+            this.tbEditor.TextChanged += new System.EventHandler(this.tbEditor_TextChanged);
+            this.tbEditor.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEditor_KeyDown);
+            // 
+            // lblFile
+            // 
+            this.lblFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFile.AutoSize = true;
+            this.lblFile.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFile.ForeColor = System.Drawing.Color.White;
+            this.lblFile.Location = new System.Drawing.Point(14, 424);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(35, 14);
+            this.lblFile.TabIndex = 1;
+            this.lblFile.Text = "File";
             // 
             // Form1
             // 
@@ -51,10 +68,13 @@ namespace texxt_editor
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(2)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFile);
             this.Controls.Add(this.tbEditor);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "Form1";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "texxt editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,7 +82,8 @@ namespace texxt_editor
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbEditor;
+        private System.Windows.Forms.RichTextBox tbEditor;
+        private System.Windows.Forms.Label lblFile;
     }
 }
 
--- texxt-editor-src/texxt-editor/Form1.cs
+++ texxt-editor-src/texxt-editor/Form1.cs
@@ -13,13 +13,28 @@ namespace texxt_editor
 {
     public partial class Form1 : Form
     {
-        private bool saved = false;
+        private bool saved = true;
 
         private string txtLocation = "";
+
+        public string TextLocation
+        {
+            get
+            {
+                return txtLocation;
+            }
+            set
+            {
+                txtLocation = value;
+                lblFile.Text = "file: " + txtLocation;
+            }
+        }
 
         public Form1()
         {
             InitializeComponent();
+
+            tbEditor.SelectionBackColor = Color.FromArgb(230, 230, 230);
 
             var args = Environment.GetCommandLineArgs();
 
@@ -27,6 +42,10 @@ namespace texxt_editor
             {
                 // Load from the file
                 OpenFile(args[1]);
+            }
+            else
+            {
+                lblFile.Text = "no file";
             }
         }
 
@@ -37,13 +56,123 @@ namespace texxt_editor
                 using (StreamReader streamReader = new StreamReader(location))
                 {
                     tbEditor.Text = streamReader.ReadToEnd();
-                    tbEditor.SelectionStart = tbEditor.Text.Length != 0 ? tbEditor.Text.Length - 1 : 0;
-                    txtLocation = location;
+                    // Set cursor location to the end
+                    tbEditor.SelectionStart = tbEditor.Text.Length != 0 ? tbEditor.Text.Length : 0;
+                    TextLocation = location;
+                    saved = true;
+                    this.Text = "texxt editor";
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show($"there was an error: {e.Message.ToLower()}");
+            }
+        }
+
+        void SaveFile()
+        {
+            try
+            {
+                if (TextLocation != "")
+                {
+                    // Save it to text location
+                    using (StreamWriter streamWriter = new StreamWriter(TextLocation))
+                    {
+                        streamWriter.Write(tbEditor.Text.Trim());
+                        saved = true;
+                        this.Text = "texxt editor";
+                    }
+                }
+                else
+                {
+                    SaveAs();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"there was an error: {e.Message.ToLower()}");
+            }
+        }
+
+        void SaveAs()
+        {
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.DefaultExt = "txt";
+                    saveFileDialog.Filter = "Text FIles|*.txt|All Files|*.*";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string fileLocation = saveFileDialog.FileName;
+
+                        using (StreamWriter streamWriter = new StreamWriter(fileLocation))
+                        {
+                            streamWriter.Write(tbEditor.Text.Trim());
+                            TextLocation = fileLocation;
+                            saved = true;
+                            this.Text = "texxt editor";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"there was an error: {e.Message.ToLower()}");
+            }
+        }
+
+        private void tbEditor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (
+                tbEditor.GetLineFromCharIndex(tbEditor.SelectionStart) == 0 && e.KeyData == Keys.Up ||
+                tbEditor.GetLineFromCharIndex(tbEditor.SelectionStart) == tbEditor.GetLineFromCharIndex(tbEditor.TextLength) && e.KeyData == Keys.Down ||
+                tbEditor.SelectionStart == tbEditor.TextLength && e.KeyData == Keys.Right ||
+                tbEditor.SelectionStart == 0 && e.KeyData == Keys.Left ||
+                (tbEditor.SelectionStart == 0 && e.KeyData == Keys.Back && tbEditor.SelectedText == "")
+            ) 
+            { 
+                e.Handled = true;
+            }
+
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                SaveFile();
+            }
+            else if (e.Control && e.KeyCode == Keys.O)
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Text FIles|*.txt|All Files|*.*";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        OpenFile(openFileDialog.FileName);
+                    }
+                }
+            }
+        }
+
+        private void tbEditor_TextChanged(object sender, EventArgs e)
+        {
+            if (saved != false)
+            {
+                saved = false;
+                this.Text += " *";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (saved == false && e.CloseReason == CloseReason.UserClosing)
+            {
+                // Ask to save the file
+                if (MessageBox.Show("The file has not been saved. Do you want to save?", "Not saved", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    SaveFile();
+                    e.Cancel = true;
+                }
             }
         }
     }
